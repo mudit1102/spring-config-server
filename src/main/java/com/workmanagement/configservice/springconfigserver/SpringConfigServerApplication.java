a66e6a66362ceff022ba7273b02e91bcdc241a94
Mudit <mudit@Mudits-MacBook-Air.local>
2020-02-02 17:35:10 +0530

    Added some configuration changes

--- src/main/java/com/workmanagement/configservice/springconfigserver/SpringConfigServerApplication.java
+++ src/main/java/com/workmanagement/configservice/springconfigserver/SpringConfigServerApplication.java
@@ -3,13 +3,15 @@ package com.workmanagement.configservice.springconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringConfigServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringConfigServerApplication.class, args);
+  }
 
 }
